4e3488848365fc8a97f39db92440635df1ac6c3f
mse2321 <mse233@gmail.com>
2015-08-08 12:54:46 -0400

    finished js code

--- app.js
+++ app.js
@@ -124,6 +124,8 @@ var getUnanswered = function(tags) {
 	});
 };
 
+// takes a string of semi-colon separated tags to be searched
+// for on StackOverflow
 var getInspiration = function(tags) {
 	
 	// the parameters we need to pass in our request to StackOverflow's API
